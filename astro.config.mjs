02f7ba3f90d88b1f945d63869f2df59db91c308f
IsaacVC82 <drawcommissions@gmail.com>
2025-02-04 10:29:06 -0600

    Preparando para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: "/https://github.com/IsaacVC82/astro1/",
+    output: "static", // Importante para GitHub Pages
+  });
