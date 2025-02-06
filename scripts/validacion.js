d70e092cdc31bd324406fc4f7e99a2b435cc61a0
IsaacVC82 <drawcommissions@gmail.com>
2025-02-06 12:41:59 -0600

    Commit 06/02/25 correccion

--- scripts/validacion.js
+++ scripts/validacion.js
@@ -1,50 +1,31 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-  validarFormulario(event);
-});
-
 function validarFormulario(event) {
-  const nombre = document.getElementById("nombre");
-  const email = document.getElementById("email");
-  const mensaje = document.getElementById("mensaje");
-  const celular = document.getElementById("celular");
-
-  const nombreTrimmed = nombre.value.trim();
-  let valido = true;
-
-  // Validación del nombre (mínimo 3 caracteres)
-  if (nombreTrimmed.length < 3|| nombreTrimmed > 128) {
-    alert("El nombre debe tener al menos 3 caracteres.");
-    valido = false;
+    const nombre = document.getElementById("nombre");
+    const email = document.getElementById("email");
+    const nombreTrimmed = nombre.value.trim();
+    let valido = true;
+  
+    // Validación del nombre (mínimo 3 caracteres)
+    if (nombreTrimmed.length < 3) {
+      alert("El nombre debe tener al menos 3 caracteres.");
+      valido = false;
+    }
+  
+    // Validación de solo letras y espacios
+    if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombreTrimmed)) {
+      alert("El nombre solo debe contener letras y espacios, sin números ni símbolos.");
+      valido = false;
+    }
+  
+    // Validación del correo electrónico
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!regexEmail.test(email.value)) {
+      alert("Por favor, ingresa un correo electrónico válido.");
+      valido = false;
+    }
+  
+    // Si hay errores, detener el envío del formulario
+    if (!valido) {
+      event.preventDefault();
+    }
   }
-
-  // Validación de solo letras y espacios
-  if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombreTrimmed)) {
-    alert("El nombre solo debe contener letras y espacios, sin números ni símbolos.");
-    valido = false;
-  }
-
-  // Validación del correo electrónico
-  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  if (!regexEmail.test(email.value)&& email.value.length <128 || email.value.length > 10) {
-    alert("Por favor, ingresa un correo electrónico válido.");
-    valido = false;
-  }
-
-  // Validación del celular (solo números y 12 caracteres)
-  const regexCelular = /^[0-9]{12}$/;
-  if (!regexCelular.test(celular.value)) {
-    alert("El celular debe contener exactamente 12 números.");
-    valido = false;
-  }
-
-  // Si hay errores, detener el envío del formulario
-  if (!valido) {
-    event.preventDefault();
-    return;
-  }
-
-  window.location.href = "gracias/index.html";
-}
-
-
-
+  
